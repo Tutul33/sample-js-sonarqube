f5bd12568405d0cc1d5c01de5ae4934ef291877e
Ubuntu <ubuntu@ip-172-31-18-3.eu-north-1.compute.internal>
2025-05-30 19:56:14 +0000

    Commit all data

---
+++ fibonacci.test.js
@@ -0,0 +1,21 @@
+const fibonacci = require('./fibonacci');
+
+test('fibonacci of 0 is 0', () => {
+  expect(fibonacci(0)).toBe(0);
+});
+
+test('fibonacci of 1 is 1', () => {
+  expect(fibonacci(1)).toBe(1);
+});
+
+test('fibonacci of 5 is 5', () => {
+  expect(fibonacci(5)).toBe(5);
+});
+
+test('fibonacci of 10 is 55', () => {
+  expect(fibonacci(10)).toBe(55);
+});
+
+test('throws error for negative input', () => {
+  expect(() => fibonacci(-1)).toThrow('Input must be a non-negative integer');
+});
